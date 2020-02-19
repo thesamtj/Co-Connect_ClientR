24dff7150626d8913ce1525bf70eb5239f515057
Samuel Tijani <omalitijani@gmail.com>
2020-02-19 08:39:39 +0100

    Define theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
 import Navbar from './components/navbar'
@@ -10,10 +12,28 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Signup from './pages/signup';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
+    }
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
           <Router>
           <Navbar />
             <div className="container">
@@ -24,7 +44,8 @@ class App extends Component {
               </Switch>
             </div>
           </Router>
-      </div>
+        </div>
+      </MuiThemeProvider>
      );
   }
 }
