f50ebf1e6e0416d3248de48c6ff4b56d86f8b483
Samuel Tijani <omalitijani@gmail.com>
2020-02-19 17:13:23 +0100

    Create scream

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Grid from '@material-ui/core/Grid';
+import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Scream from "../components/Scream";
 
 class Home extends Component {
-    state = {
-        screams: null
-    }
+  state = {
+    screams: null
+  };
 
-    componentDidMount(){
-        axios.get('/screams')
-            .then(res => {
-                console.log(res.data)
-                this.setState({
-                    screams: res.data
-                })
-            })
-            .catch(err => console.log(err));
-    }
+  componentDidMount() {
+    axios
+      .get("/screams")
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          screams: res.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
-    render() {
-    let recentScreamsMarkup = this.state.screams ? (this.state.screams.map(scream => <p>{scream.body}</p>)) : <Scream scream={scream}/>
-        return (
-            <div>
-                <Grid container spacing={2}>
-                    <Grid item sm={8} xs={12}>
-                        {recentScreamsMarkup}
-                    </Grid>
-                    <Grid item sm={4} xs={12}>
-                        <p>Profile...</p>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <div>
+        <Grid container spacing={2}>
+          <Grid item sm={8} xs={12}>
+            {recentScreamsMarkup}
+          </Grid>
+          <Grid item sm={4} xs={12}>
+            <p>Profile...</p>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
