2f4a24654b81155fcbf011da0d1408969d4a592c
Samuel Tijani <omalitijani@gmail.com>
2020-02-19 15:46:04 +0100

    Get screams

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // Components
@@ -25,6 +25,9 @@ const theme = createMuiTheme({
       main: '#ff3d00',
       dark: '#b22a00',
       contrastText: '#fff'
+    },
+    typography: {
+      useNextVariants: true
     }
   }
 });
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 
 class Home extends Component {
+    state = {
+        screams: null
+    }
+
+    componentDidMount(){
+        axios.get('/screams')
+            .then(res => {
+                console.log(res.data)
+                this.setState({
+                    screams: res.data
+                })
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
+    let recentScreamsMarkup = this.state.screams ? (this.state.screams.map(scream => <p>{scream.body}</p>)) : <Scream scream={scream}/>
         return (
             <div>
-                <h1>Home page</h1>
+                <Grid container spacing={2}>
+                    <Grid item sm={8} xs={12}>
+                        {recentScreamsMarkup}
+                    </Grid>
+                    <Grid item sm={4} xs={12}>
+                        <p>Profile...</p>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
