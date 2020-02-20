3c8c06af677edc5fa6bdb6045629a8f18f2293ec
Samuel Tijani <omalitijani@gmail.com>
2020-02-20 10:44:49 +0100

    Set user actions

--- src/pages/login.js
+++ src/pages/login.js
@@ -22,36 +22,17 @@ class Login extends Component {
     this.state = {
       email: "",
       password: "",
-      loading: false,
       errors: {}
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({
-      loading: true
-    });
     const userData = {
       email: this.state.email,
       password: this.state.password
     };
-    axios
-      .post("/login", userData)
-      .then(res => {
-        console.log(res.data);
-        localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-        this.setState({
-          loading: false
-        });
-        this.props.history.push("/");
-      })
-      .catch(err => {
-        this.setState({
-          errors: err.response.data,
-          loading: false
-        });
-      });
+    
   };
 
   handleChange = event => {
