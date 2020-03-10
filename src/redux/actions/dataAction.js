09682ccdda41c065f42f126d4b0baeb97bd69126
Samuel Tijani <omalitijani@gmail.com>
2020-03-10 14:40:58 +0100

    Add scream details

--- src/redux/actions/dataAction.js
+++ src/redux/actions/dataAction.js
@@ -32,6 +32,20 @@ export const getScreams = () => dispatch => {
       });
     });
 };
+// Get a scream
+export const getScream = (screamId) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .get(`/scream/${screamId}`)
+    .then((res) => {
+      dispatch({
+        type: SET_SCREAM,
+        payload: res.data
+      });
+      dispatch({ type: STOP_LOADING_UI });
+    })
+    .catch((err) => console.log(err));
+};
 
 // Post a scream
 export const postScream = (newScream) => (dispatch) => {
@@ -86,4 +100,8 @@ export const deleteScream = screamId => dispatch => {
       dispatch({ type: DELETE_SCREAM, payload: screamId });
     })
     .catch(err => console.log(err));
+};
+
+export const clearErrors = () => (dispatch) => {
+  dispatch({ type: CLEAR_ERRORS });
 };
