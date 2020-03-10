bf4882587424f52d6caa401f05d07f4ad304a871
Samuel Tijani <omalitijani@gmail.com>
2020-03-10 15:05:17 +0100

    Add like button

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,32 +1,35 @@
-import React, { Component } from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import PropTypes from "prop-types";
-import MyButton from '../util/MyButton';
-//MUI Stuff
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import PropTypes from 'prop-types';
+import MyButton from '../../util/MyButton';
+import DeleteScream from './DeleteScream';
+import ScreamDialog from './ScreamDialog';
+import LikeButton from './LikeButton';
+// MUI Stuff
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 // Icons
 import ChatIcon from '@material-ui/icons/Chat';
 // Redux
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
 
 const styles = {
   card: {
     position: 'relative',
-    display: "flex",
+    display: 'flex',
     marginBottom: 20
   },
   image: {
-    minWidth: 200,
-    objectFit: "cover"
+    minWidth: 200
   },
   content: {
-    padding: 25
+    padding: 25,
+    objectFit: 'cover'
   }
 };
 
@@ -43,9 +46,17 @@ class Scream extends Component {
         screamId,
         likeCount,
         commentCount
+      },
+      user: {
+        authenticated,
+        credentials: { handle }
       }
     } = this.props;
 
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeleteScream screamId={screamId} />
+      ) : null;
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -62,10 +73,22 @@ class Scream extends Component {
           >
             {userHandle}
           </Typography>
+          {deleteButton}
           <Typography variant="body2" color="textSecondary">
             {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
+          <LikeButton screamId={screamId} />
+          <span>{likeCount} Likes</span>
+          <MyButton tip="comments">
+            <ChatIcon color="primary" />
+          </MyButton>
+          <span>{commentCount} comments</span>
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
         </CardContent>
       </Card>
     );
@@ -79,7 +102,7 @@ Scream.propTypes = {
   openDialog: PropTypes.bool
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   user: state.user
 });
 
